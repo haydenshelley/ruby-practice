1c8e1817713194462f1041835631a8b98c0cfbb3
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-07-28 13:51:37 -0700

    add to days array

--- data_munging.rb
+++ data_munging.rb
@@ -7,7 +7,34 @@
 days = [
   {day: 1, max: 88, min: 59}, 
   {day: 2, max: 76, min: 63},
-  {day: 3, max: 77, min: 55}
+  {day: 3, max: 77, min: 55},
+  {day: 4, max: 77, min: 59},
+  {day: 5, max: 90, min: 66},
+  {day: 6, max: 81, min: 61}, 
+  {day: 7, max: 73, min: 57},
+  {day: 8, max: 75, min: 54},
+  {day: 9, max: 86, min: 32},
+  {day: 10, max: 84, min: 64},
+  {day: 11, max: 91, min: 59}, 
+  {day: 12, max: 88, min: 73},
+  {day: 13, max: 70, min: 59},
+  {day: 14, max: 61, min: 59},
+  {day: 15, max: 70, min: 59},
+  {day: 16, max: 79, min: 59}, 
+  {day: 17, max: 81, min: 57},
+  {day: 18, max: 79, min: 55},
+  {day: 19, max: 81, min: 55},
+  {day: 20, max: 84, min: 55},
+  {day: 21, max: 86, min: 59}, 
+  {day: 22, max: 90, min: 63},
+  {day: 23, max: 90, min: 55},
+  {day: 24, max: 90, min: 55},
+  {day: 25, max: 90, min: 55},
+  {day: 26, max: 90, min: 59}, 
+  {day: 27, max: 90, min: 63},
+  {day: 28, max: 97, min: 55},
+  {day: 29, max: 77, min: 55},
+  {day: 30, max: 77, min: 55}
 ]
 
 i = 0
